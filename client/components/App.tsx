78d8af633a5d4b4aecb9b00ba0a9545b63e14e30
student <student@pop-os.localdomain>
2023-11-03 09:44:06 +1300

    make stationary pingu

--- client/components/App.tsx
+++ client/components/App.tsx
@@ -1,8 +1,13 @@
+import BouncingBall from "./Ball"
+
 function App() {
   return (
     <div>
       <h1>App</h1>
       <p>React development has begun!</p>
+      <div>
+        <BouncingBall />
+      </div>
     </div>
   )
 }
