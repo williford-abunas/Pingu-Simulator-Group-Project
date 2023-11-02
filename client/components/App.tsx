0548a6546d254b256120ebc164542353c1d326e9
Cody Ilg <codyilg@gmail.com>
2023-11-03 12:45:21 +1300

    css update

--- client/components/App.tsx
+++ client/components/App.tsx
@@ -1,21 +1,24 @@
 import Pingu from './Pingu'
+import '../main.css'
+import Header from './Header.tsx'
+import Footer from './Footer.tsx'
 // import Ball from './Ball'
 function App() {
-  
-    return (
-      <>
+  return (
+    <>
+      <Header />
+      <div className="winterWonderland">
         <div>
           <h1>App</h1>
           <p>React development has begun!</p>
         </div>
-        
+
         {/* <Ball /> */}
         <Pingu />
-      </>
-  
-    )
-  
-  
+      </div>
+      <Footer />
+    </>
+  )
 }
 
 export default App
