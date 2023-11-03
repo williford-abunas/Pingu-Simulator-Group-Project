240ecb73650dcc8b6c26ec8e1203d5a78e91f965
Raidon Ramiah <142761066+raidon-ramiah@users.noreply.github.com>
2023-11-03 14:17:11 +1300

    Add more pingus

--- client/components/App.tsx
+++ client/components/App.tsx
@@ -2,10 +2,12 @@ import Pingu from './Pingu'
 import '../main.css'
 import Header from './Header.tsx'
 import Footer from './Footer.tsx'
+import { useState } from 'react'
 // import Ball from './Ball'
 function App() {
-  function makeMore(){
-    console.log("hoy")
+  const [pinguArr, setPinguArr] = useState([<Pingu />])
+  function makeMore() {
+    setPinguArr([...pinguArr, <Pingu />])
   }
   return (
     <>
@@ -14,12 +16,11 @@ function App() {
         <div></div>
 
         {/* <Ball /> */}
-        <Pingu />
+        {[pinguArr]}
 
-        <button onClick={makeMore}>asdf</button>
+        <button onClick={makeMore}>Make more characters</button>
 
         <Footer />
-
       </div>
     </>
   )
